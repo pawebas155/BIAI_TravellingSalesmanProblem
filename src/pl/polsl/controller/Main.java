c1c279a489c8b2feaa71bf2098403b9c1b3a4ea9
Mateusz Jagoda <mateusz.jagoda@itsilesia.com>
2019-05-08 21:57:59 +0200

    Copy classes from old repo, fix packages

---
+++ src/pl/polsl/controller/Main.java
@@ -0,0 +1,11 @@
+package pl.polsl.controller;
+
+import pl.polsl.model.TSPGraph;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TSPGraph graph = new TSPGraph("data/qa194.txt");
+
+    }
+}
