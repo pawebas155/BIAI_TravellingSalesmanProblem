82e441365ac9c204c16536ed5b5893daa26aca0e
Mateusz Jagoda <mateusz.jagoda@itsilesia.com>
2019-05-17 15:54:49 +0200

    Implement needed variables

--- src/pl/polsl/model/Algorithm.java
+++ src/pl/polsl/model/Algorithm.java
@@ -1,8 +1,6 @@
 package pl.polsl.model;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 public class Algorithm {
 
@@ -11,41 +9,51 @@ public class Algorithm {
     TSPGraph graph;
     private Population population;
 
-    public Algorithm(int populationSize, String fileName, int percOfMutation){
+    public Algorithm(int populationSize, String fileName, int percOfMutation) {
         graph = new TSPGraph(fileName);
         population = new Population(populationSize, graph);
         percentOfMutation = percOfMutation;
     }
 
-    public void evaluation(){
+    public void evaluation() {
         population.calculateFitnessForAllIndividuals();
         population.sortIndividualsReversed();
         population.calculateProbability();
     }
 
-    public Individual selection(){
+    public Individual selection() {
         return population.getRandomIndividualByProbability();
     }
 
-    public Individual crossover(){
-        return null;
+    public Individual crossover(Individual firstParent, Individual secondParent) {
+        Integer currentNode, nextNode;
+        Individual child = new Individual(graph);
+        List<List<Integer>> neighbourList = new ArrayList<List<Integer>>();
+
+        for(int i = 0; i< graph.getNumberOfCities(); i++)
+        {
+            neighbourList.add(new ArrayList<Integer>());
+        }
+
+
+        return child;
+
     }
 
-    public void mutation(){
+    public void mutation() {
 
         int generationSize = population.getGenerationSize();
         Random random = new Random();
         Set mutatedElement = new HashSet();
         //Individual individual;
 
-        for(int i = 0; i < ((percentOfMutation * generationSize) / (100)); i++){
+        for (int i = 0; i < ((percentOfMutation * generationSize) / (100)); i++) {
             int n = random.nextInt(generationSize);
-            if(mutatedElement.add(n) == true){
+            if (mutatedElement.add(n) == true) {
                 population.getIndividualById(n).swapTwoRandomCities();
                 //individual = population.getIndividualById(n);
                 //individual.swapTwoRandomCities();
-            }
-            else{
+            } else {
                 i--;
             }
         }
@@ -53,7 +61,7 @@ public class Algorithm {
     }
 
 
-    public void Evolve(){
+    public void Evolve() {
 
     }
 }
